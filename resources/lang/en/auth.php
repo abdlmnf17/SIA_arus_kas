1f6daecc0f390212bb5acc01296af980f04e216a
abdlmnf17 <11210195@bsi.ac.id>
2024-06-22 00:53:43 +0700

    Memperbaiki data validasi

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -13,8 +13,8 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
+    'failed' => 'Gagal masuk, bisa jadi email tidak terdaftar / kata sandi salah.',
+    'password' => 'Kata sandi kamu salah.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
 ];
